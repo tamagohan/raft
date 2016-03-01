77fcfed22fa01c0e742b31ca46d0fd5d346818d4
tamagohan <fantastipo23@gmail.com>
2016-03-01 23:02:10 +0900

    Start node process with all server names

--- lib/node_cluster.ex
+++ lib/node_cluster.ex
@@ -9,13 +9,18 @@ defmodule Raft.NodeCluster do
 
   def init(number_of_nodes) do
     children = [worker(Raft.Node, [], restart: :temporary)]
-    opts = [strategy: :simple_one_for_one, name: Hoge]
+    opts     = [strategy: :simple_one_for_one]
     {:ok, sup} = Supervisor.start_link(children, opts)
 
-    for _ <- 1..number_of_nodes do
-      {:ok, _pid} = Supervisor.start_child(sup, [])
-    end
-    {:ok, sup}
+    node_names = generate_node_names(number_of_nodes)
+    Enum.map(node_names, fn node_name ->
+      node_names_without_self = List.delete(node_names, node_name)
+      {:ok, _pid} = Supervisor.start_child(sup, [node_name, node_names_without_self])
+    end)
+    {:ok, node_names}
+  end
+
+  defp generate_node_names(number_of_nodes) when is_integer(number_of_nodes) do
+    for i <- 1..number_of_nodes do "node#{i}" |> String.to_atom end
   end
 end
-
